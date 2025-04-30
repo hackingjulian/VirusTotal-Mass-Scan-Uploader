444963313fedef23b7ac3385125ed26afc88dac7
Julian M <154713872+hackingjulian@users.noreply.github.com>
2025-04-30 16:36:19 -0400

    Create vtTool.py

---
+++ vtTool.py
@@ -0,0 +1,111 @@
+import requests 
+import csv
+import pyfiglet
+
+ascii_art = pyfiglet.figlet_format("VirusToal Bulk Uploader")
+print("\033[1;32m" + ascii_art + "\033[0m")
+print("\033[1;36m{:>10}\033[0m : {}".format("Author", "Julian Moodie"))
+
+
+print("Enter Api Key")
+apikey = input()
+
+print("enter file name")
+fileName = input()
+
+print("hash, ip or domain...")
+var = input()
+
+if var == "hash":
+    try:
+        with open(fileName, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+    except:
+        print("Error reading file")
+        
+        for row in reader:
+            target = row['target'].strip()
+            url = f'https://www.virustotal.com/api/v3/files/{target}'
+
+            headers = {'x-apikey': apikey}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 429:
+                print("Quota exceeded https://docs.virustotal.com/docs/higher-quota")
+                break
+            else:
+                data = response.json()['data']['attributes']
+                stats = data['last_analysis_stats']
+                if stats['malicious'] > 0:
+                    print(f"{target} => Malicious: {stats['malicious']} Vendors")
+                    check = True
+                if stats['suspicious'] > 0:
+                    if check == True:
+                        None
+                else:
+                    print(f"{target} => suspicious: {stats['suspicious']} Vendors")
+
+    pass
+
+if var == "ip":
+    try:
+        with open(fileName, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+    except:
+        print("Error reading file")
+    with open(fileName, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            target = row['target'].strip()
+            url = f'https://www.virustotal.com/api/v3/ip_addresses/{target}'
+
+            headers = {'x-apikey': apikey}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 429:
+                print("Quota exceeded https://docs.virustotal.com/docs/higher-quota")
+                break
+            else:
+                data = response.json()['data']['attributes']
+                stats = data['last_analysis_stats']
+                if stats['malicious'] > 0:
+                    print(f"{target} => Malicious: {stats['malicious']} Vendors")
+                    check = True
+                if stats['suspicious'] > 0:
+                    if check == True:
+                        None
+                else:
+                    print(f"{target} => suspicious: {stats['suspicious']} Vendors")
+
+
+else:
+    pass
+
+if var == "domain":
+    try:
+        with open(fileName, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+    except:
+        print("Error reading file")
+    with open(fileName, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            target = row['target'].strip()
+            url = f'https://www.virustotal.com/api/v3/domains/{target}'
+
+            headers = {'x-apikey': apikey}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 429:
+                print("Quota exceeded https://docs.virustotal.com/docs/higher-quota")
+                break
+            else:
+                data = response.json()['data']['attributes']
+                stats = data['last_analysis_stats']
+                if stats['malicious'] > 0:
+                    print(f"{target} => Malicious: {stats['malicious']} Vendors")
+                    check = True
+                if stats['suspicious'] > 0:
+                    if check == True:
+                        None
+                else:
+                    print(f"{target} => suspicious: {stats['suspicious']} Vendors")
+
+
